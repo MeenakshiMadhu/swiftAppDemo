e0ed0040b7048f3b878482574b23de83eb580562
Meenakshi Madhu <meenakshimadhu00@gmail.com>
2025-06-02 19:45:27 -0400

    Initial Commit

---
+++ demoApp3/demoApp3App.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp3App.swift
+//  demoApp3
+//
+//  Created by Meenakshi Madhu on 6/2/25.
+//
+
+import SwiftUI
+
+@main
+struct demoApp3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ demoApp3Tests/demoApp3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp3Tests.swift
+//  demoApp3Tests
+//
+//  Created by Meenakshi Madhu on 6/2/25.
+//
+
+import Testing
+@testable import demoApp3
+
+struct demoApp3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
