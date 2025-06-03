0dcd8e58ae4e33676c285e1a76b482f9b8f4287a
Meenakshi Madhu <meenakshimadhu00@gmail.com>
2025-06-02 21:47:11 -0400

    card view app

--- demoApp3/ContentView.swift
+++ demoApp3/ContentView.swift
@@ -9,13 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            Color(.systemMint)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 20) {
+                
+                Image("niagarafalls")
+                    .resizable()
+                    .cornerRadius(16)
+                    .aspectRatio(contentMode: .fit)
+                    
+                
+                HStack {
+                    Text("Niagara Falls")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.black)
+                    
+                    Spacer()
+                    
+                    VStack() {
+                        HStack {
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star")
+                        }
+                        Text("Reviews (3656)")
+                    }.foregroundColor(Color.orange)
+                        .font(.caption)
+                }
+                
+                Text("Come visit the falls to witness nature's magnificence!")
+                    .foregroundColor(Color.black)
+               
+                HStack {
+                    Spacer()
+                    Image(systemName: "binoculars.fill")
+                    Image(systemName: "fork.knife")
+                    Image(systemName: "fuelpump.fill")
+                }
+                .foregroundColor(Color.gray)
+                .font(.caption)
+            }
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(Color.white)
+                .cornerRadius(15))
+            .shadow(radius: 15)
+            .padding()
+            
         }
-        .padding()
+        
+        
+        
     }
 }
 
